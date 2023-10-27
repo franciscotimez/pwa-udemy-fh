75a1d41e929c5b795cc1418d2aa09b1f5f9b1ca3
Francisco Timez <franciscotimez@gmail.com>
2023-10-27 17:40:07 -0300

    SW: Sin conexion a internet

--- 04-cache-offline/sw.js
+++ 04-cache-offline/sw.js
@@ -1,3 +1,13 @@
+// Service Worker
 
+self.addEventListener("fetch", (event) => {
+  const offlineResp = new Response(`
+  Bienvenido a mi pagina Web
 
+  Disculpa, pero para usarla, necesitas internet.
+  `);
 
+  const resp = fetch(event.request).catch(() => offlineResp);
+
+  event.respondWith(resp);
+});
