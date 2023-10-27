a8c2ed863c1c6d8b94c965615e2c528eda561a09
Francisco Timez <franciscotimez@gmail.com>
2023-10-27 14:57:22 -0300

    inicio con service worker

---
+++ 02-service-worker/js/app.js
@@ -0,0 +1,10 @@
+// App Logic
+
+// if ("serviceWorker" in navigator) {
+//   console.log("Podemos usarlo!");
+// }
+
+// confirmar si podemos usar el SW
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/sw.js");
+}
---
+++ 02-service-worker/sw.js
@@ -0,0 +1,9 @@
+// Service Worker
+
+self.addEventListener("fetch", (event) => {
+  const resp = fetch(event.request).then((resp) => {
+    return resp.ok ? resp : fetch("img/main.jpg");
+  });
+
+  event.respondWith(resp);
+});
