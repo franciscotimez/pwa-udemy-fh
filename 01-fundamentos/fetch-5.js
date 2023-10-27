bc5b5292fc71a0fab0878371633f6e21fae8b16a
Francisco Timez <franciscotimez@gmail.com>
2023-10-27 09:46:59 -0300

    FetchAPI error handle

--- 01-fundamentos/fetch-5.js
+++ 01-fundamentos/fetch-5.js
@@ -1,18 +1,16 @@
 // Fetch API Clone
 
-fetch("https://reqres.in/api/users/1").then((resp) => {
-  resp.clone().json()
-    .then((usuario) => {
-      console.log(usuario.data);
-    });
-
-    resp.clone().json()
-    .then((usuario) => {
-      console.log(usuario.data);
-    });
-
-    resp.json()
-    .then((usuario) => {
-      console.log(usuario.data);
-    });
-});
+fetch("https://reqres.in/api/users/1000")
+  .then((resp) => {
+    if (resp.ok) {
+      return resp.json();
+    } else {
+      // console.log('No existe el usuario 1000');
+      throw new Error("No existe el usuario 1000");
+    }
+  })
+  .then(console.log)
+  .catch((error) => {
+    console.log("Error en la petición");
+    console.log(error);
+  });
