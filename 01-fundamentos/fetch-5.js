0a8dea4cee4c9bedcea8f8e96c96501e4d16c720
Francisco Timez <franciscotimez@gmail.com>
2023-10-27 09:42:44 -0300

    FetchAPI clone Example

---
+++ 01-fundamentos/fetch-5.js
@@ -0,0 +1,18 @@
+// Fetch API Clone
+
+fetch("https://reqres.in/api/users/1").then((resp) => {
+  resp.clone().json()
+    .then((usuario) => {
+      console.log(usuario.data);
+    });
+
+    resp.clone().json()
+    .then((usuario) => {
+      console.log(usuario.data);
+    });
+
+    resp.json()
+    .then((usuario) => {
+      console.log(usuario.data);
+    });
+});
