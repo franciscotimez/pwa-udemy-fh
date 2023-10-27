303d49fcb44bcbf9b6eb82418b6bd6c2d05234e4
Francisco Timez <franciscotimez@gmail.com>
2023-10-27 09:37:40 -0300

    FetchAPI Get Image and create url blob

---
+++ 01-fundamentos/fetch-4.js
@@ -0,0 +1,11 @@
+// FetchAPI GET image
+
+let img = document.querySelector("img");
+
+fetch("superman.png")
+  .then((resp) => resp.blob())
+  .then((imagen) => {
+    // console.log(imagen);
+    var imgPath = URL.createObjectURL(imagen);
+    img.src = imgPath;
+  });
