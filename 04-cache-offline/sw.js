bbdae54f5b69748c224609a84f5e14ded29eae65
Francisco Timez <franciscotimez@gmail.com>
2023-11-06 12:44:23 -0300

    Network with cache fallback

--- 04-cache-offline/sw.js
+++ 04-cache-offline/sw.js
@@ -2,7 +2,7 @@
 // const CACHE_NAME = "cache-1";
 const CACHE_STATIC_NAME = "static-v2";
 const CACHE_DYNAMIC_NAME = "dynamic-v1";
-const CACHE_DYNAMIC_ITEMS = 5;
+const CACHE_DYNAMIC_LIMIT = 5;
 const CACHE_INMUTABLE_NAME = "inmutable-v1";
 
 const limpiarCache = (cacheName, numberOfItems) => {
@@ -41,19 +41,36 @@ self.addEventListener("fetch", (event) => {
   // // 1 - Cache Only
   // event.respondWith(caches.match(event.request));
 
-  // 2 - Cache with Network Fallback
-  const response = caches.match(event.request).then((res) => {
-    if (res) return res;
+  // // 2 - Cache with Network Fallback
+  // const response = caches.match(event.request).then((res) => {
+  //   if (res) return res;
 
-    // No existe -> Network Fallback
-    return fetch(event.request).then((newRes) => {
+  //   // No existe -> Network Fallback
+  //   return fetch(event.request).then((newRes) => {
+  //     caches.open(CACHE_DYNAMIC_NAME).then((cache) => {
+  //       cache.put(event.request, newRes);
+  //       limpiarCache(CACHE_DYNAMIC_NAME, CACHE_DYNAMIC_LIMIT);
+  //     });
+  //     return newRes.clone();
+  //   });
+  // });
+
+  // event.respondWith(response);
+
+  // 3 - Network first with cache fallback
+  const response = fetch(event.request)
+    .then((newRes) => {
+      if (!newRes) return caches.match(event.request);
+
       caches.open(CACHE_DYNAMIC_NAME).then((cache) => {
         cache.put(event.request, newRes);
-        limpiarCache(CACHE_DYNAMIC_NAME, CACHE_DYNAMIC_ITEMS);
+        limpiarCache(CACHE_DYNAMIC_NAME, CACHE_DYNAMIC_LIMIT);
       });
       return newRes.clone();
+    })
+    .catch((err) => {
+      return caches.match(event.request);
     });
-  });
 
   event.respondWith(response);
 });
