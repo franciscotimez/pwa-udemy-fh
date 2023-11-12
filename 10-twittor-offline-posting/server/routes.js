51447d1941dd53f31012e7809c9e50ddf092aa98
Francisco Timez <Francisco@Timez>
2023-11-12 18:31:49 -0300

    get messajes

--- 10-twittor-offline-posting/server/routes.js
+++ 10-twittor-offline-posting/server/routes.js
@@ -2,9 +2,18 @@
 var express = require('express');
 var router = express.Router();
 
+const mensajes = [
+  {
+    _id: "xxx",
+    user: "spiderman",
+    mensaje: "Hola mundo"
+  }
+]
+
 // Get mensajes
 router.get('/', function (req, res) {
-  res.json('Obteniendo mensajes');
+  // res.json('Obteniendo mensajes');
+  res.json(mensajes)
 });
 
 module.exports = router;
