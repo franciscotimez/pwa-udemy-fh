a9ce809ccd0894595678f7258a3fa9f1995360c8
Francisco Timez <franciscotimez@gmail.com>
2023-11-13 12:28:02 -0300

    Guardar Suscripciones

--- 11-twittor-con-push/server/push.js
+++ 11-twittor-con-push/server/push.js
@@ -1,14 +1,16 @@
+const fs = require("fs");
 const URLSafeBase64 = require("urlsafe-base64");
 const vapid = require("./vapid.json");
 
-const suscripciones = []
+const suscripciones = require("./subs-db.json");
 
 module.exports.getKey = () => {
   return URLSafeBase64.decode(vapid.publicKey);
 };
 
 module.exports.addSubscription = (suscripcion) => {
-  suscripciones.push(suscripcion)
+  suscripciones.push(suscripcion);
 
+  fs.writeFileSync(`${__dirname}/subs-db.json`, JSON.stringify(suscripciones));
   console.log(suscripciones);
 };
